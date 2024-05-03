d92dac5437ae9cb6d068fa37ca305e9570857cf9
chrisvalere03gmail.com <nz-valere>
2024-05-03 12:09:07 +0100

    form

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+$('.xy').on('click', function(){
+    // $('h2').html('<strong>orange</strong>');
+    $('#form').slideToggle().animate({fontSize: '25px'});
+    $('.xy').css('display','none');
+});
