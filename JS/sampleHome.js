6ad087a9b27914f9054aa6780008e1d50ee33568
chrisvalere03gmail.com <nz-valere>
2024-06-10 14:27:33 +0100

    10 june

--- JS/sampleHome.js
+++
@@ -1,9 +0,0 @@
-var root = document.documentElement;
-const lists = document.querySelectorAll('.hs'); 
-
-lists.forEach(el => {
-  const listItems = el.querySelectorAll('li');
-  const n = el.children.length;
-  el.style.setProperty('--total', n);
-});
-
